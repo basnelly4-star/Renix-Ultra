031082d32b1a1b0404853c56defd8490e650c97d
basnelly4-star <basnelly4@gmail.com>
2026-05-26 12:04:04 +0100

    Update AddBalanceModal.tsx

--- src/components/AddBalanceModal.tsx
+++ src/components/AddBalanceModal.tsx
@@ -8,9 +8,9 @@ import { supabase } from "@/integrations/supabase/client";
 
 const FEE_PERCENT = 2;
 const BANK_DETAILS = {
-  bankName: "Renmoney ",
+  bankName: "Sparkle ",
   accountName: "Micheal Micheal ",
-  accountNumber: "3608496838",
+  accountNumber: "1003140448",
 };
 
 export const AddBalanceModal = ({ open, onOpenChange, onSuccess }: any) => {
