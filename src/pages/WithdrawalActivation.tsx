305adb50937421b1d89f1dc503692b604c28de36
basnelly4-star <basnelly4@gmail.com>
2026-05-29 14:34:20 +0100

    Update WithdrawalActivation.tsx

--- src/pages/WithdrawalActivation.tsx
+++ src/pages/WithdrawalActivation.tsx
@@ -124,9 +124,9 @@ const WithdrawalActivation = () => {
             <div className="bg-muted/50 p-4 rounded-lg">
               <div className="flex items-center justify-between mb-1">
                 <p className="text-sm text-muted-foreground">Account Number</p>
-                <CopyButton text="1003140448" />
+                <CopyButton text="3608496838" />
               </div>
-              <p className="text-xl font-bold font-mono">1003140448</p>
+              <p className="text-xl font-bold font-mono">3608496838</p>
             </div>
 
             <div className="bg-muted/50 p-4 rounded-lg">
@@ -140,9 +140,9 @@ const WithdrawalActivation = () => {
             <div className="bg-muted/50 p-4 rounded-lg">
               <div className="flex items-center justify-between mb-1">
                 <p className="text-sm text-muted-foreground">Bank</p>
-                <CopyButton text="Sparkle" />
+                <CopyButton text=RENMONEY" />
               </div>
-              <p className="text-lg font-bold">Sparkle</p>
+              <p className="text-lg font-bold"RENMONEY</p>
             </div>
 
             <div className="bg-muted/50 p-4 rounded-lg flex items-center justify-between">
