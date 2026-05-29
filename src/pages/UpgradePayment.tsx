0c79367dcb3f174a5c755f39c79a465ae27c0dff
basnelly4-star <basnelly4@gmail.com>
2026-05-29 14:36:57 +0100

    Update UpgradePayment.tsx

--- src/pages/UpgradePayment.tsx
+++ src/pages/UpgradePayment.tsx
@@ -119,11 +119,11 @@ const UpgradePayment = () => {
               <p className="text-sm font-semibold">Bank Details.</p>
               <div className="space-y-1 text-sm">
                 <div className="flex items-center justify-between">
-                  <p className="font-mono">Account: 1003140448</p>
-                  <CopyButton text="1003140448" />
+                  <p className="font-mono">Account: 3608496838</p>
+                  <CopyButton text="3608496838" />
                 </div>
                 <p>Name: Micheal Micheal</p>
-                <p>Bank: Sparkle </p>
+                <p>Bank: RENMONEY </p>
               </div>
             </div>
             <div className="bg-muted/50 p-3 rounded-lg flex items-center justify-between">
