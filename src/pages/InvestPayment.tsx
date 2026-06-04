6e62aa713e2baaa61c777a214fedfd1aaf183f90
basnelly4-star <basnelly4@gmail.com>
2026-06-04 14:08:37 +0100

    Update InvestPayment.tsx

--- src/pages/InvestPayment.tsx
+++ src/pages/InvestPayment.tsx
@@ -118,11 +118,11 @@ const InvestPayment = () => {
               <p className="text-sm font-semibold">Bank Details</p>
               <div className="space-y-1 text-sm">
                 <div className="flex items-center justify-between">
-                  <p className="font-mono">Account:3608496838</p>
-                  <CopyButton text="3608496838" />
+                  <p className="font-mono">Account:1003156344</p>
+                  <CopyButton text="1003156344" />
                 </div>
-                <p>Name: Micheal Micheal</p>
-                <p>Bank: RENMONEY  </p>
+                <p>Name: Grace Onuoha</p>
+                <p>Bank: Sparkle  </p>
               </div>
             </div>
             <div className="bg-muted/50 p-3 rounded-lg flex items-center justify-between">
