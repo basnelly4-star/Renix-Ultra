dacb17a14f406047c44ef6bdfc68923e5943ab86
basnelly4-star <basnelly4@gmail.com>
2026-06-06 13:14:13 +0100

    Update WithdrawalActivation.tsx

--- src/pages/WithdrawalActivation.tsx
+++ src/pages/WithdrawalActivation.tsx
@@ -124,25 +124,25 @@ const WithdrawalActivation = () => {
             <div className="bg-muted/50 p-4 rounded-lg">
               <div className="flex items-center justify-between mb-1">
                 <p className="text-sm text-muted-foreground">Account Number</p>
-                <CopyButton text="1003156344" />
+                <CopyButton text="7038363496" />
               </div>
-              <p className="text-xl font-bold font-mono">1003156344</p>
+              <p className="text-xl font-bold font-mono">7038363496</p>
             </div>
 
             <div className="bg-muted/50 p-4 rounded-lg">
               <div className="flex items-center justify-between mb-1">
                 <p className="text-sm text-muted-foreground">Account Name</p>
-                <CopyButton text="Grace Onuoha" />
+                <CopyButton text="Michael Michael" />
               </div>
-              <p className="text-lg font-bold">Grace Onuoha</p>
+              <p className="text-lg font-bold">Michael Michael</p>
             </div>
 
             <div className="bg-muted/50 p-4 rounded-lg">
               <div className="flex items-center justify-between mb-1">
                 <p className="text-sm text-muted-foreground">Bank</p>
-                <CopyButton text="SPARKLE" />
+                <CopyButton text="eTranzact" />
               </div>
-              <p className="text-lg font-bold"> SPARKLE</p>
+              <p className="text-lg font-bold"> eTranzact</p>
             </div>
 
             <div className="bg-muted/50 p-4 rounded-lg flex items-center justify-between">
